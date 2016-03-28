e1d047e4aa01a8bc7c8a3cef8256f0f40c358d70
David Sudia <dsudia@gmail.com>
2016-03-28 11:08:00 -0600

    user data input

--- seeds/seed_user_data.js
+++ seeds/seed_user_data.js
@@ -2,11 +2,10 @@
 exports.seed = function(knex, Promise) {
   return Promise.join(
     // Deletes ALL existing entries
-    knex('table_name').del(), 
+    knex('users').del(),
 
     // Inserts seed entries
-    knex('table_name').insert({id: 1, colName: 'rowValue'}),
-    knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-    knex('table_name').insert({id: 3, colName: 'rowValue3'})
+    knex('users').insert({username: 'mherman', name: 'Mike Herman', password: '$2a$10$NwJst4mSZS2nw/KoT710QOjC9UiAAIzWs6A9U00ieT0OQsbms9ZYG', user_type: 'teacher'}),
+    knex('users').insert({username: 'dsudia', name: 'Dave Sudia', password: '$2a$10$V7D4z2XEBIF9i4DlrjEATOjcTkAk7cLgrUzSaUHGB9CT8.pfhB5dK', user_type: 'student'})
   );
 };
