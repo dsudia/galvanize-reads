1ce5b6ddf6ec21bac83586ae92aa55b2090861b1
David Sudia <dsudia@gmail.com>
2016-03-28 11:00:47 -0600

    book seed data input

--- migrations/20160328103539_create_books_table.js
+++ migrations/20160328103539_create_books_table.js
@@ -2,6 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('books', function(table) {
     table.increments();
+    table.string('title');
     table.string('genre');
     table.text('description');
     table.string('image_url');
