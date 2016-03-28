89ca3dedaf79de992aa8ea274625a1658af116b2
David Sudia <dsudia@gmail.com>
2016-03-28 11:05:07 -0600

    relationship seed data input

--- seeds/seed_relationship_data.js
+++ seeds/seed_relationship_data.js
@@ -2,11 +2,16 @@
 exports.seed = function(knex, Promise) {
   return Promise.join(
     // Deletes ALL existing entries
-    knex('table_name').del(), 
+    knex('author_book_rels').del(),
 
     // Inserts seed entries
-    knex('table_name').insert({id: 1, colName: 'rowValue'}),
-    knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-    knex('table_name').insert({id: 3, colName: 'rowValue3'})
+    knex('author_book_rels').insert({author_id: knex('authors').select('id').where('first_name', 'Alex'), book_id: knex('books').select('id').where('title', 'Python In A Nutshell')}),
+    knex('author_book_rels').insert({author_id: knex('authors').select('id').where('first_name', 'Anna'), book_id: knex('books').select('id').where('title', 'Python In A Nutshell')}),
+    knex('author_book_rels').insert({author_id: knex('authors').select('id').where('first_name', 'Steve'), book_id: knex('books').select('id').where('title', 'Python In A Nutshell')}),
+    knex('author_book_rels').insert({author_id: knex('authors').select('id').where('first_name', 'Allen B.'), book_id: knex('books').select('id').where('title', 'Think Python')}),
+    knex('author_book_rels').insert({author_id: knex('authors').select('id').where('first_name', 'Bonnie'), book_id: knex('books').select('id').where('title', 'Learning React Native')}),
+    knex('author_book_rels').insert({author_id: knex('authors').select('id').where('first_name', 'Kyle'), book_id: knex('books').select('id').where('title', 'You Don\'t Know JS: ES6 & Beyond')}),
+    knex('author_book_rels').insert({author_id: knex('authors').select('id').where('first_name', 'Kyle'), book_id: knex('books').select('id').where('title', 'You Don\'t Know JS: Scope & Closures')}),
+    knex('author_book_rels').insert({author_id: knex('authors').select('id').where('first_name', 'Kyle'), book_id: knex('books').select('id').where('title', 'You Don\'t Know JS: Async & Performance')})
   );
 };
